a981017d826faa17fd521cb301c674fb84f5d728
DariusPirvulescu <organicdarius@gmail.com>
2020-07-02 23:04:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("server: port 3000")
+});
