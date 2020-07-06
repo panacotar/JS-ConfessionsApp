07195fcf5ce177df919bad6256d3a127d0c41068
DariusPirvulescu <organicdarius@gmail.com>
2020-07-06 22:09:11 +0100

    implemented level 1 security, added post routes for register and login, added mongoose and .env file, connected to local mongoDB, created userSchema

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,19 +13,63 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/", function(req, res) {
+mongoose.connect("mongodb://" + process.env.DB_HOST + "/confessionsUserDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema )
+
+app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", (req, res) => {
   res.render("register")
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post("/register", (req, res) => {
+  const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+  })
 
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("confessions");  
+    }
+  })  
+});
+
+app.post("/login", (req, res) => {
+  const inputEmail = req.body.username;
+  const inputPass = req.body.password;
+
+  User.findOne({ email: inputEmail }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser){
+        if (foundUser.password === inputPass) {
+          res.render("confessions")
+        }
+      }
+    }
+  })
+})
 
 
 
