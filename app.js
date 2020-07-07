5de99f8f8c631dde82d22517cb17995c7f3fcb9e
DariusPirvulescu <organicdarius@gmail.com>
2020-07-07 23:36:55 +0100

    added level 3 encryption using md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 const app = express();
 
@@ -26,9 +26,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.DB_SECRET_STRING, encryptedFields: ["password"] })
-
 const User = new mongoose.model("User", userSchema )
 
 app.get("/", (req, res) => {
@@ -46,7 +43,7 @@ app.get("/login", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
   })
 
   newUser.save((err) => {
@@ -60,7 +57,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const inputEmail = req.body.username;
-  const inputPass = req.body.password;
+  const inputPass = md5(req.body.password);
 
   User.findOne({ email: inputEmail }, (err, foundUser) => {
     if (err) {
@@ -69,7 +66,10 @@ app.post("/login", (req, res) => {
       if (foundUser){
         if (foundUser.password === inputPass) {
           res.render("confessions")
-        }
+        } else {
+          res.render("login")
+          console.log("wrong credentials")
+        } 
       }
     }
   })
