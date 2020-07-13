8b437002c69f818790e5cdf1a88bf3324c4bea24
DariusPirvulescu <organicdarius@gmail.com>
2020-07-13 21:43:47 +0100

    changed the port to dynamic, local/heroku

--- app.js
+++ app.js
@@ -120,8 +120,10 @@ app.post("/login", (req, res) => {
 });
 
 
-
-
-app.listen(3000, function() {
-  console.log("server: port 3000")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("server at port 3000")
 });
