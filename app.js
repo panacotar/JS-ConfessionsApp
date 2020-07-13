b12593168185ae89a1bfe6297e22625724653bc5
DariusPirvulescu <organicdarius@gmail.com>
2020-07-14 00:32:11 +0100

    changed callbackUrl to heroku for google and facebook strategies

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/confessions",
+  callbackURL: "https://confessions-app.herokuapp.com/auth/google/confessions",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
@@ -89,7 +89,7 @@ function(accessToken, refreshToken, profile, cb) {
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/confessions"
+  callbackURL: "https://confessions-app.herokuapp.com/auth/facebook/confessions"
   },
   function (accessToken, refreshToken, profile, cb) {
       // console.log(profile);
