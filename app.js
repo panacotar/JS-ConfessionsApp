19158ef4c8c554e1f153a0a3f91e8b284e9d8cad
DariusPirvulescu <organicdarius@gmail.com>
2020-07-14 00:26:02 +0100

    switched to mongoose.connect to remote DB

--- app.js
+++ app.js
@@ -29,21 +29,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Local DB
-mongoose.connect("mongodb://" + process.env.DB_HOST + "/confessionsUserDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://" + process.env.DB_HOST + "/confessionsUserDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-// // Remote DB
-// mongoose.connect(
-//   "mongodb+srv://dario-admin:" +
-//     process.env.DB_PASS +
-//     "@cluster0-bhjc9.mongodb.net/confessionsDB",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
+// Remote DB
+mongoose.connect(
+  "mongodb+srv://dario-admin:" +
+    process.env.DB_PASS +
+    "@cluster0-bhjc9.mongodb.net/confessionsDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.set("useCreateIndex", true);
 
